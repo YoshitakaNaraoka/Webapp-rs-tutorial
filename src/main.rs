aff7c273153df100cd5545fc18a51dc70faf336a
Placeless <mikoprpr@hotmail.com>
2025-02-27 19:36:54 +0900

    Learning routing & paste Yew sample

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Actix Web のエントリーポイントになる予定
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+/*
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -14,14 +15,22 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+*/
+
+async fn front() -> impl Responder {
+    HttpResponse::Ok().body("Yew app")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            /*
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            */
+            .route("/web/src/main.rs", web::get().to(front))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
