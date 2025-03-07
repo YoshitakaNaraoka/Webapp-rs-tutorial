3d618d5926c79306f4102ff5762870197916039c
Placeless <mikoprpr@hotmail.com>
2025-03-08 08:08:59 +0900

    Upgrade stylist crate & Gemini 2.0 flash: define global style macro

--- tauri/src/style.rs
+++ tauri/src/style.rs
@@ -1,4 +1,4 @@
-use stylist::{style, Style};
+use stylist::{global_style, Style};
 use yew::prelude::*;
 
 #[derive(Clone, PartialEq, Properties)]
@@ -6,38 +6,37 @@ pub struct BackgroundComponentProps {
     pub background_state: bool,
 }
 
+global_style!(
+    r#"
+    :host {
+        font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+        font-size: 16px;
+        line-height: 24px;
+        font-weight: 400;
+
+        color: #0f0f0f;
+        background-color: #f6f6f6;
+
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-text-size-adjust: 100%;
+    }
+    .light_mode {
+        color: #0f0f0f;
+        background-color: #f6f6f6;
+    }
+
+    .dark_mode {
+        color: #f6f6f6;
+        background-color: #2f2f2f;
+    }
+    "#
+);
+
 #[function_component(BackgroundComponent)]
 pub fn button_bg_component(props: &BackgroundComponentProps) -> Html {
-    let style = style!(
-        r#"
-        :host {
-            font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-            font-size: 16px;
-            line-height: 24px;
-            font-weight: 400;
-
-            color: #0f0f0f;
-            background-color: #f6f6f6;
-
-            font-synthesis: none;
-            text-rendering: optimizeLegibility;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-            -webkit-text-size-adjust: 100%;
-        }
-        .light_mode {
-            color: #0f0f0f;
-            background-color: #f6f6f6;
-        }
-
-        .dark_mode {
-            color: #f6f6f6;
-            background-color: #2f2f2f;
-        }
-        "#
-    )
-    .expect("Failed to create style"); // エラーハンドリングを追加
-
     let class = if props.background_state {
         "dark_mode"
     } else {
@@ -45,7 +44,6 @@ pub fn button_bg_component(props: &BackgroundComponentProps) -> Html {
     };
 
     let mut classes = Classes::new();
-    classes.push(style.get_class_name());
     classes.push(class);
 
     html! {
